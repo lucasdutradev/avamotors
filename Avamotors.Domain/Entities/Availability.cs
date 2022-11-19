9de10e943d1237921332d8d5e53ed06380cf23e1
Lucas Bryan <lucasbryan0217@gmail.com>
2022-11-18 22:20:51 -0300

    feat: create method add Availability in car

--- Avamotors.Domain/Entities/Availability.cs
+++ Avamotors.Domain/Entities/Availability.cs
@@ -2,5 +2,19 @@ namespace Avamotors.Domain.Entities;
 
 public class Availability : Entitie
 {
+	public Availability(DateTime date, decimal priceInThisDay)
+	{
+		Date = date;
+		PriceInThisDay = priceInThisDay;
+		FilledDate = false;
+	}
+
 	public DateTime Date { get; private set; }
+	public decimal PriceInThisDay { get; private set; }
+	public bool FilledDate { get; private set; }
+
+	public void checkData()
+	{
+		FilledDate = true;
+	}
 }
--- Avamotors.Domain/Entities/Car.cs
+++ Avamotors.Domain/Entities/Car.cs
@@ -1,13 +1,10 @@
-using Avamotors.Domain.Shared.VOs;
-
 namespace Avamotors.Domain.Entities;
 
 public class Car : Entitie
 {
-	public Car(Seller owner, double priceDay, Name name, double km, string year, string model, string description, string image, Client client)
+	private readonly IList<Availability> _availabilitys;
+	public Car(string name, double km, string year, string model, string description, string image, Client? client)
 	{
-		Owner = owner;
-		PriceDay = priceDay;
 		Name = name;
 		Km = km;
 		Year = year;
@@ -15,15 +12,52 @@ public class Car : Entitie
 		Description = description;
 		Image = image;
 		Client = client;
+		_availabilitys = new List<Availability>();
 	}
 
 	public Seller Owner { get; private set; }
-	public double PriceDay { get; private set; }
-	public Name Name { get; private set; }
+	public string Name { get; private set; }
 	public double Km { get; private set; }
 	public string Year { get; private set; }
 	public string Model { get; private set; }
 	public string Description { get; private set; }
 	public string Image { get; private set; }
-	public Client Client { get; private set; }
+	public Client? Client { get; private set; }
+	public IEnumerable<Availability> Availabilitys => _availabilitys.ToArray();
+
+	public bool AddCarASeller(Seller owner)
+	{
+		if (owner.IsValid)
+		{
+			Owner = owner;
+			return true;
+		}
+		return false;
+
+	}
+	public bool AddAvailability(DateTime date, decimal priceInThisDay)
+	{
+		var existThisAvailability = Availabilitys.FirstOrDefault(x => x.Date.Date == date.Date);
+
+		if (existThisAvailability == null)
+		{
+			var newAvailability = new Availability(date, priceInThisDay);
+			_availabilitys.Add(newAvailability);
+			return true;
+		}
+		return false;
+	}
+
+
+	public bool AddCarAClient(Client client, Guid availability_id)
+	{
+		var availability = Availabilitys.FirstOrDefault(x => x.Id == availability_id);
+		if (client.IsValid && !availability.FilledDate)
+		{
+			availability.checkData();
+			Client = client;
+			return true;
+		}
+		return false;
+	}
 }
